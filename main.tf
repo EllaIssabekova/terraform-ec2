6cc0bb16b209d715393103a45505ddec85be7809
EllaIssabekova <eissabekova1@gmail.com>
2022-04-06 17:30:47 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = var.region
 }
 
-resource "aws_key_pair" "my-key" {
+#resource "aws_key_pair" "my-key" {
   key_name   = var.key_name
   public_key = var.public_key
 }
@@ -13,7 +13,7 @@ resource "aws_instance" "web" {
   ami           = var.ami_id
   security_groups = [aws_security_group.web.id]
 
-  subnet_id     = tolist(data.aws_subnet_ids.my-subnets.ids)[0]
+ # subnet_id     = tolist(data.aws_subnet_ids.my-subnets.ids)[0]
 
   tags = {
     Name = "${var.environment}-instance"
